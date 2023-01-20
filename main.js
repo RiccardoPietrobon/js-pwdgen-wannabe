a1242b662b55fe414b7ee82f762b11ca04f24ac9
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-01-20 16:07:47 +0100

    Completato

---
+++ main.js
@@ -0,0 +1,42 @@
+//richiesta nome
+let name = prompt("Qual è il tuo nome?");
+
+//richiesta nome
+let surname = prompt("Qual è il tuo cognome?");
+
+//richiesta nome
+let color = prompt("Qual è il tuo colore preferito?");
+
+//cifra costante
+const number = 23;
+
+// messaggio finale
+const welcome_msg = `${name}${surname}${color}${number}`;
+document.getElementById('my_message').innerHTML = welcome_msg;
+
+
+//piccolo bonus
+
+//richiesta nazione
+let stato = prompt("Qual è la tua nazione?")
+//mia nazione
+const my_nazione = `${stato}`;
+document.getElementById('nazione').innerHTML = my_nazione;
+
+//richiesta regione
+let stato = prompt("Qual è la tua regione?")
+//mia regione
+const my_regione = `${regione}`;
+document.getElementById('regione').innerHTML = my_nazione;
+
+//richiesta nazione
+let stato = prompt("Qual è la tua nazione?")
+//mia nazione
+const my_nazione = `${stato}`;
+document.getElementById('nazione').innerHTML = my_nazione;
+
+//richiesta nazione
+let stato = prompt("Qual è la tua nazione?")
+//mia nazione
+const my_nazione = `${stato}`;
+document.getElementById('nazione').innerHTML = my_nazione;
